36e8f0d501bebabf4b7c4f94fe6419890df3dcf5
Yanka28 <yanachepeluk@ukr.net>
2024-02-18 23:41:38 +0200

    comm4

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import * as React from 'react';
-// import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store';
 import App from './App.jsx';
-import Catalog from 'pages/Catalog';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
       <BrowserRouter basename="/adverts">
         <App />
       </BrowserRouter>
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
